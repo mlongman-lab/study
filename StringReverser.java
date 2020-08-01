5818385a348e2a03c0dff89bf57cb5ebb924c6c8
mlongman-lab <68370926+mlongman-lab@users.noreply.github.com>
2020-08-01 10:26:00 +0300

    Add files via upload

---
+++ StringReverser.java
@@ -0,0 +1,10 @@
+public class StringReverser {
+    public static void main(String[] args) {
+        String st = "Задача1";
+        StringBuilder sSt = new StringBuilder(st);
+        sSt.reverse();
+        System.out.println(sSt);
+    }
+
+}
+
